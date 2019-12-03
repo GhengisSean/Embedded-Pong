7223c1e27c1faea3fb58df2db3ce76901a946c2a
tlabiak <54776399+tlabiak@users.noreply.github.com>
2019-12-03 02:12:42 -0500

    slave update

--- Main.c
+++ Main.c
@@ -28,14 +28,14 @@
 #define PI 3.14159265
 
 #define PADDLEHEIGHT					25
-#define PADDLEX								20
-#define PADDLEY								107
+#define PADDLEX								10
+#define PADDLEY								117
 
 extern Sema4Type LCDFree;
 uint16_t origin[2]; 	// The original ADC value of x,y if the joystick is not touched, used as reference
 int16_t x = 63;  			// horizontal position of the crosshair, initially 63
 int16_t y = 63;  			// vertical position of the crosshair, initially 63
-int16_t prevx, prevy = 63;	// Previous x and y values of the crosshair
+int16_t prevx, prevy =63;	// Previous x and y values of the crosshair
 uint8_t select;  			// joystick push
 uint8_t area[2];
 uint32_t PseudoCount;
@@ -59,10 +59,7 @@ void World_Init(void);
 // Ball stuff ;)
 int16_t ball_x = 63;
 int16_t ball_y = 63;
-int ball_speed = 7;
-int ball_angle = 0;
-int x_speed;
-int y_speed;
+
 int16_t ball_xold = 63;
 int16_t ball_yold = 63;
 
@@ -82,16 +79,16 @@ void Device_Init(void){
 int UpdatePosition(uint16_t rawx, uint16_t rawy, jsDataType* data){
 
 	if (rawy < origin[1]){
-		y = y + ((origin[1] - rawy) >> 5);
+		y = y + ((origin[1] - rawy) >> 4);
 	}
 	else{
-		y = y - ((rawy - origin[1]) >> 5);
+		y = y - ((rawy - origin[1]) >> 4);
 	}
 
-	if (y > 127 - CROSSSIZE){
-		y = 127 - CROSSSIZE;}
-	if (y < 0){
-		y = 0;}
+	if (y > 127 - PADDLEHEIGHT/2){
+		y = 127 - PADDLEHEIGHT/2;}
+	if (y < 0 + PADDLEHEIGHT/2){
+		y = 0 + PADDLEHEIGHT/2;}
 	data->y = y;
 	return 1;
 }
@@ -103,10 +100,13 @@ void Producer(void){
 	BSP_Joystick_Input(&rawX,&rawY,&select);
 	UpdateWork += UpdatePosition(rawX,rawY,&data); // calculation work
 	data.x = l_paddle_y;
-	while (1) { // Recv from Master
+	while (1) { // Recv from master
 		int data1 = UART_Recv();
 		if (data1 == 0xFE) {
 			break;
+		}
+		else if (data1 == 0xFA){
+			World_Init();
 		}
 		else if (data1 == 0xFF){
 			count = 0;
@@ -125,7 +125,7 @@ void Producer(void){
 		}
 	}
 	JsFifo_Put(data); // send to consumer 
-	UART_Send(data.y); // Send To Master
+	UART_Send(data.y); // Send To master
 	
 }
 
@@ -194,58 +194,40 @@ void Consumer(void){
 }
 
 
-// ***********ButtonWork2*************
-void Restart(void){
-	uint32_t StartTime,CurrentTime,ElapsedTime, ButtonStartTime;
-	NumSamples = RUNLENGTH; // first kill the foreground threads
-	OS_Sleep(50); // wait
-	StartTime = OS_MsTime();
-	ElapsedTime = 0;
-	ButtonStartTime = OS_Time();
-	OS_bWait(&LCDFree);
-	BSP_LCD_FillScreen(BGCOLOR);
-	Button2RespTime = (Button2RespTime + (OS_Time() - ButtonStartTime)) >> 1;
-	while (ElapsedTime < 500){
-		CurrentTime = OS_MsTime();
-		ElapsedTime = CurrentTime - StartTime;
-		BSP_LCD_DrawString(5,6,"Restarting",LCD_WHITE);
-	}
-	BSP_LCD_FillScreen(BGCOLOR);
-	OS_bSignal(&LCDFree);
-	World_Init();
-	// restart
-
-  NumSamples = 0;
-  UpdateWork = 0;
-
-	PseudoCount = 0;
-	x = 63; y = 63;
-	NumCreated += OS_AddThread(&Consumer,128,1); 
-	//NumCreated += OS_AddThread(&Display,128,3);
-  OS_Kill();  // done, OS does not return from a Kill
-} 
 
 //************SW2Push*************
 void SW2Push(void){
   if(OS_MsTime() > 20 ){ // debounce
-    if(OS_AddThread(&Restart,128,4)){
-			OS_ClearMsTime();
-      NumCreated++; 
-    }
+		UART_Send(0xFA);
+		World_Init();
     OS_ClearMsTime();  // at least 20ms between touches
   }
 	
 }
 
 void World_Init(void){
+	x = 63;
+	y = 63;
+	ball_x = 63;
+  ball_y = 63;
+	l_paddle_y = 63;
+  r_paddle_y = 63;
 	BSP_LCD_FillScreen(BGCOLOR);
 	origin[0] = 0x7FF;
 	origin[1] = 0x7FF;
+	
+	BSP_LCD_DrawFastVLine(PADDLEX, prevx - 12, PADDLEHEIGHT, LCD_BLACK); //draw left paddle
+	BSP_LCD_DrawFastVLine(PADDLEY, prevy - 12, PADDLEHEIGHT, LCD_BLACK); //draw right paddle
+	
+	BSP_LCD_DrawBall(ball_xold, ball_yold, LCD_BLACK);
 	
 	BSP_LCD_DrawFastVLine(PADDLEX, l_paddle_y - 12, PADDLEHEIGHT, LCD_RED); //draw left paddle
 	BSP_LCD_DrawFastVLine(PADDLEY, r_paddle_y - 12, PADDLEHEIGHT, LCD_GREEN); //draw right paddle
 	
 	BSP_LCD_DrawBall(ball_x, ball_y, LCD_WHITE);
+	
+	ball_xold = ball_x;
+	ball_yold = ball_y;
 
 }
 
